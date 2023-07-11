7ef5d0b13c657048ca70d69639219198d4e36176
masterkudi <cudini.andrea@gmail.com>
2023-07-11 21:01:52 +0200

    esercizio completato

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,12 +1,8 @@
-/**
- * @type {HTMLSelectElement}
- */
+
 const squareCellsSelect = document.querySelector("[name='square-cells']");
 const btnPlay = document.querySelector("#btn-play");
 
-/**
- * @type {HTMLElement}
- */
+
 const gridContainer = document.querySelector(".grid-container");
 
 btnPlay.addEventListener("click", onBtnClick);
@@ -24,4 +20,47 @@ function onBtnClick() {
     printGrid(gridContainer, gridList);
 
 }
+
+
+function createSingleSquare(squareContent, squareCells) {
+    const square = document.createElement("div");
+  
+    const squaresPerRow = Math.sqrt(squareCells);
+  
+    square.classList.add("grid-square");
+    square.textContent = squareContent;
+    square.style.flexBasis = `calc(100% / ${squaresPerRow})`;
+  
+    square.addEventListener("click", function () {
+      square.classList.toggle("bg-primary");
+    });
+  
+    // Output
+    return square;
+  }
+  
+
+  function createGrid(squaresNumber) {
+    const grid = [];
+  
+    for (let i = 0; i < squaresNumber; i++) {
+      // salvo in una variabile l'output della funzione createSingleSquare
+      const newSquare = createSingleSquare("" + i, squaresNumber);
+  
+      grid.push(newSquare);
+    }
+  
+    // Output
+    return grid;
+  }
+  
+ 
+  function printGrid(container, squaresList) {
+    // reset del contenuto del container per evitare che ci siano altri div creati precedentemente
+    container.innerHTML = "";
+  
+    for (let i = 0; i < squaresList.length; i++) {
+      container.append(squaresList[i]);
+    }
+  }
 
